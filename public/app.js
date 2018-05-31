f128160e4e86ba98f17ff335f21367e74fe5d45f
stevenhorkey <stevenhorkey@gmail.com>
2018-05-31 11:59:56 -0700

    fully add and delete articles from saved

--- public/app.js
+++ public/app.js
@@ -1,99 +1,66 @@
-// Grab the articles as a json
-$.getJSON("/articles", function(data) {
+$.getJSON("/api/articles", function(data) {
   // For each one
+  console.log(data)
   for (var i = 0; i < data.length; i++) {
-    // Display the apropos information on the page
-    // $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-    $("#articles").append("<div class='col-lg-3 col-md-6 mb-4'><div class='card'><img class='card-img-top' src='http://placehold.it/500x325' alt=''><div class='card-body'><h4 class='card-title'>"+data[i].title+"</h4><p class='card-text'>"+data[i].link+"</p></div><div class='card-footer'><a data-id="+data[i]._id+" href='#' class='btn btn-primary'>Save Article</a></div></div></div>");
+    $("#articles").append("<div class='col-lg-3 col-md-6 mb-4'><div class='card' data-id="+data[i]._id+"><img class='card-img-top' src='"+data[i].image+"' alt=''><div class='card-body'><a href='"+data[i].link+"'><h4 class='card-title'>"+data[i].title+"</h4></a><p class='card-text'></p></div><div class='card-footer'><a data-id="+data[i]._id+" class='save-btn btn btn-primary'>Save Article</a></div></div></div>");
   }
 });
 
-$('#scrape-btn').on('click',function(e){
+$.getJSON("/api/saved", function(data) {
+  // For each one
+  console.log(data)
+  for (var i = 0; i < data.length; i++) {
+    $("#saved-articles").append("<div class='col-lg-3 col-md-6 mb-4'><div class='card' data-id="+data[i]._id+"><img class='card-img-top' src='"+data[i].image+"' alt=''><div class='card-body'><a href='"+data[i].link+"'><h4 class='card-title'>"+data[i].title+"</h4></a><p class='card-text'></p></div><div class='card-footer'><a data-id="+data[i]._id+" class='text-white del-save-btn btn btn-primary'>Delete From Saved</a></div></div></div>");
+  }
+});
+
+$('#scrape-btn').on('click',function(){
   $.ajax({
     method: "GET",
-    url: "/scrape/"
+    url: "/scrape"
   })
-    // With that done, add the note information to the page
     .then(function(data) {
       console.log(data);
-      // // The title of the article
-      // $("#notes").append("<h2>" + data.title + "</h2>");
-      // // An input to enter a new title
-      // $("#notes").append("<input id='titleinput' name='title' >");
-      // // A textarea to add a new note body
-      // $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-      // // A button to submit a new note, with the id of the article saved to it
-      // $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-
-      // // If there's a note in the article
-      // if (data.note) {
-      //   // Place the title of the note in the title input
-      //   $("#titleinput").val(data.note.title);
-      //   // Place the body of the note in the body textarea
-      //   $("#bodyinput").val(data.note.body);
-      // }
     });
 })
 
-// Whenever someone clicks a p tag
-$(document).on("click", "p", function() {
-  // Empty the notes from the note section
-  $("#notes").empty();
-  // Save the id from the p tag
-  var thisId = $(this).attr("data-id");
+$('#articles').on("click",'.save-btn', function() {
+  var thisId = $(this).closest('div.card').attr("data-id");
+  var thisCard = $(this).closest('div.card');
+  console.log(thisCard.find('h4').text());
 
-  // Now make an ajax call for the Article
   $.ajax({
-    method: "GET",
-    url: "/articles/" + thisId
+    method: "PUT",
+    url: "/api/saved/" + thisId,
+  
   })
-    // With that done, add the note information to the page
     .then(function(data) {
       console.log(data);
-      // The title of the article
-      $("#notes").append("<h2>" + data.title + "</h2>");
-      // An input to enter a new title
-      $("#notes").append("<input id='titleinput' name='title' >");
-      // A textarea to add a new note body
-      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-      // A button to submit a new note, with the id of the article saved to it
-      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-
-      // If there's a note in the article
-      if (data.note) {
-        // Place the title of the note in the title input
-        $("#titleinput").val(data.note.title);
-        // Place the body of the note in the body textarea
-        $("#bodyinput").val(data.note.body);
-      }
+      location.reload();
     });
+
 });
 
-// When you click the savenote button
-$(document).on("click", "#savenote", function() {
-  // Grab the id associated with the article from the submit button
-  var thisId = $(this).attr("data-id");
+$('#saved-articles').on("click",'.del-save-btn', function() {
+  console.log('working')
+  var thisId = $(this).closest('div.card').attr("data-id");
 
-  // Run a POST request to change the note, using what's entered in the inputs
   $.ajax({
-    method: "POST",
-    url: "/articles/" + thisId,
-    data: {
-      // Value taken from title input
-      title: $("#titleinput").val(),
-      // Value taken from note textarea
-      body: $("#bodyinput").val()
-    }
+    method: "PUT",
+    url: "/api/unsaved/" + thisId,
+  
   })
-    // With that done
     .then(function(data) {
-      // Log the response
       console.log(data);
-      // Empty the notes section
-      $("#notes").empty();
+      location.reload();
     });
 
-  // Also, remove the values entered in the input and textarea for note entry
-  $("#titleinput").val("");
-  $("#bodyinput").val("");
 });
+
+$('#saved-toggle').click(function(){
+  $('#saved-articles').slideToggle();
+})
+
+$('#show-toggle').click(function(){
+  $('#articles').slideToggle();
+})
